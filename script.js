741138a773d4a4345e219bd608285b41bb0f0163
Chii Yuen <chii.yuen@hotmail.com>
2022-12-27 21:51:51 +0800

    Add proper validation and styling

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const password = document.querySelector(".password");
 const confirmPassword = document.querySelector(".confirm");
 const form = document.querySelector("form");
-const button = document.querySelector("button");
 const span = document.querySelector(".password ~ span");
 const inputs = document.querySelectorAll("input");
 
-form.addEventListener("submit", () => {
-  if (password.value != confirmPassword.value) {
+form.noValidate = true;
+form.addEventListener("submit", e => {
+  if (!e.target.checkValidity()) {
+    e.preventDefault();
+    inputs.forEach(input => {
+      if (!input.checkValidity()) {
+        input.classList.add("invalid");
+      } else {
+        input.classList.remove("invalid");
+      }
+    })
+  };
+  if (!isPasswordSame()) {
+    e.preventDefault();
     password.classList.add("invalid");
     confirmPassword.classList.add("invalid");
-    span.textContent = "* Passwords do not match";
-    span.classList.add("invalid");
+    span.classList.add("help");
+    console.log("not same");
   } else {
     password.classList.remove("invalid");
     confirmPassword.classList.remove("invalid");
-    span.textContent = "";
-    span.classList.remove("invalid");
+    span.classList.remove("help");
+    console.log("SAME");
   }
 });
 
+
+function isPasswordSame() {
+  if (password.value !== confirmPassword.value || password.value === "" || confirmPassword.value === "") {
+    return false;
+  } else if (password.value === confirmPassword.value && password.value !== "") {
+    return true;
+  }
+}
